92d3b50152029357ffc5972743d5c1577f701dba
github username <github email address>
2017-11-13 18:23:15 -0500

    Seth and Erick Finished

---
+++ db/migrate/007_change_type_reservations.rb
@@ -0,0 +1,7 @@
+class ChangeTypeReservations < ActiveRecord::Migration
+  def change
+    change_column :reservations, :checkin, :datetime
+    change_column :reservations, :checkout, :datetime
+  end
+
+end
